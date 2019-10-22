9d7c8a691717a6dc69207a00c65802c93729456f
Nuttapat Kirawittaya <nuttapat.kirawittaya@gmail.com>
2019-10-23 02:33:02 +0800

    Auto create checks if not found

--- src/main.ts
+++ src/main.ts
@@ -266,9 +266,28 @@ async function updateCheckRun(
     ...github.context.repo
   });
 
-  const check = response.data.check_runs.find(
-    check => check.name === checkName
-  );
+  let check = response.data.check_runs.find(check => check.name === checkName);
+
+  // Bail if we have more than one check and there's no named run found
+  if (!check && response.data.check_runs.length < 2) {
+    core.debug(`Couldn't find a check run matching "${checkName}".`);
+
+    // Create new check run as we couldn't find a matching one.
+    await tools.checks.create({
+      ...github.context.repo,
+      name: checkName,
+      head_sha: github.context.sha,
+      status: 'in_progress',
+      started_at: new Date().toISOString()
+    });
+
+    const response = await tools.checks.listForRef({
+      ref: github.context.ref,
+      ...github.context.repo
+    });
+
+    check = response.data.check_runs.find(check => check.name === checkName);
+  }
 
   if (!check) {
     return core.setFailed(
